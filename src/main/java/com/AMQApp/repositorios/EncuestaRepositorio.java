ea32bb1c5d017decb241d5869eb0be242db27ba9
Matias Pavón <matiaspavon5@hotmail.com>
2022-05-19 20:48:56 -0300

    Repos Mati

--- src/main/java/com/AMQApp/repositorios/EncuestaRepositorio.java
+++ src/main/java/com/AMQApp/repositorios/EncuestaRepositorio.java
@@ -1,8 +1,14 @@
 package com.AMQApp.repositorios;
 
+import com.AMQApp.entidades.Encuesta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class EncuestaRepositorio {
+public interface EncuestaRepositorio extends JpaRepository<Encuesta, String> {
     
+    @Query("SELECT e FROM Encuesta e WHERE e.titulo LIKE :%titulo%")
+    public Encuesta buscarPorTitulo (@Param("titulo") String titulo);
 }
