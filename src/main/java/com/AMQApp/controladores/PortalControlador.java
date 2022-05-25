2867da17f59da03bf9ec2a2b18cebc3c73fcc54e
Laura de Borbón <52927859+laudebor@users.noreply.github.com>
2022-05-24 22:31:03 -0300

    Prueba registroUsuario 1

---
+++ src/main/java/com/AMQApp/controladores/PortalControlador.java
@@ -0,0 +1,13 @@
+package com.AMQApp.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller("/")
+public class PortalControlador {
+    
+    @GetMapping("/")
+    public String index(){
+        return "index.html";
+    }
+}
