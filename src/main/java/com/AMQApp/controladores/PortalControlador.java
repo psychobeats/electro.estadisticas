a757db57f6af0743f0adc59be649e0190ff82346
Laura de Borbón <laudebor@gmail.com>
2022-06-16 19:23:54 -0300

    Top5PortalControlador

--- src/main/java/com/AMQApp/controladores/PortalControlador.java
+++ src/main/java/com/AMQApp/controladores/PortalControlador.java
@@ -22,11 +22,11 @@ public class PortalControlador {
     
     @GetMapping("/")
     public String index(ModelMap modelo){
-//        try{
-//            modelo.put("querys", encuestaServicio.topFive());
-//        }catch(ErrorServicio e){
-//            modelo.put("error", e.getMessage());
-//        }
+        try{
+            modelo.put("querys", encuestaServicio.topFive());
+        }catch(ErrorServicio e){
+            modelo.put("error", e.getMessage());
+        }
         return "indexInicial.html";
     }
     
@@ -45,11 +45,11 @@ public class PortalControlador {
     
     @GetMapping("/loginExitoso")
     public String loginExito(ModelMap modelo){
-//         try{
-//            modelo.put("querys", encuestaServicio.topFive());
-//        }catch(ErrorServicio e){
-//            modelo.put("error", e.getMessage());
-//        }
+         try{
+            modelo.put("querys", encuestaServicio.topFive());
+        }catch(ErrorServicio e){
+            modelo.put("error", e.getMessage());
+        }
       return "IndexIniciado.html";
     }
     
